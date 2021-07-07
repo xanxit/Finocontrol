d40b3103e3f336d311c6d1e0c6f9184114d59898
Sanchit Hajela <sanchit10hajela@gmail.com>
2021-07-07 22:41:05 +0530

    Added Overview page

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -4,42 +4,236 @@ export default function Overview() {
     return (
       <div>
         <div className="flex justify-evenly flex-wrap">
-          <div className="flex flex-col items-center p-8 bg-white shadow-lg rounded mx-8 mb-8 flex-1 md:mb-0  md:w-1/3 sm:flex-initial">
-            <h1 className="text-indigo-500 text-4xl my-4  ">Lorem Ipsum</h1>
-            <p className=" text-justify px-8 mb-8  ">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores molestias, molestiae ex autem dicta nulla blanditiis
-              architecto cumque repudiandae quam quia impedit rerum optio
-              consectetur commodi pariatur nam eveniet dolores!
+          <div className="flex flex-col p-10 bg-gray-200 bg-opacity-20 shadow-lg rounded mx-8 mb-8 flex-1 md:mb-0  md:w-1/3 sm:flex-initial">
+            <h1 className="text-green-700 text-4xl my-4 mb-10  ">
+              Overview
+            </h1>
+            <p className="mb-8">
+              <span className="text-lg font-bold">
+                Program Duration: 15th Jan 2021-31st May 2021
+              </span>
+              <br />
+              Register Now !!!
+              <br />
+              <br />
+              The following courses are the part of the program
+              <br />
+              <br />
+              <span className="text-lg font-semibold">4 mandatory courses</span>
+              <br />
+              <br />
+              <li>Applied Financial Statement Analysis</li>
+              <li>Equity valuation certification</li>
+              <li>Investment banking certifcation</li>
+              <li>Credit Analysis Certification</li>
+              <br />
+              <br />
+              <span className="text-lg font-semibold">
+                2 Optional courses(any 2)
+              </span>
+              <li>Financial Modeling Certifcation</li>
+              <li>Applied Derivatives Certification</li>
+              <li>Applied Economics Certifcation</li>
+              <li>Finance Essentials - Banking & Markets Certification</li>
+              <br />
+              <br />
+              Each mandatory course will have:
+              <li>1 certifcation test</li>
+              <li>1 live project</li>
             </p>
           </div>
           <div className="flex flex-col justify-between mx-8 md:mx-0 md:w-1/4 ">
-            <div className="flex flex-col bg-white rounded p-4 items-center shadow-lg border-r-8 border-red-400 mb-4 md:mb-auto">
-              <h2 className="font-bold   ">Lorem Ipsum</h2>
-              <p className="p-4  text-gray-600">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
-                doloribus quia voluptates maxime totam illum iure, quis...
-              </p>
-              <div className="flex  justify-between ">
-                <div className="pill bg-gray-400 rounded-full px-4 text-xs mr-2  py-1">
-                  #NodeJS
+            <div className="flex flex-col bg-green-800 bg-opacity-10 rounded p-4  shadow-lg  mb-4 md:mb-auto">
+              <h2 className="font-bold   ">This Course includes </h2>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-2 font-bold text-green-900 ">
+                    70 hours of videos|10 hours
+                  </div>
                 </div>
-              </div>
+              </ul>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-2 font-bold text-green-900">
+                    About 150 hours of effort
+                  </div>
+                </div>
+              </ul>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-2 font-bold text-green-900">
+                    100+ articles,case studies
+                  </div>
+                </div>
+              </ul>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-2 font-bold text-green-900">
+                    Self evaluation test
+                  </div>
+                </div>
+              </ul>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-2 font-bold text-green-900">
+                    4 live projects
+                  </div>
+                </div>
+              </ul>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-3 font-bold text-green-900">
+                    Certification Test
+                  </div>
+                </div>
+              </ul>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-3 font-bold text-green-900">
+                    4 live projects
+                  </div>
+                </div>
+              </ul>
+              <ul>
+                <div className="p-8 flex flex-wrap justify-start text-gray-600">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                      />
+                    </svg>
+                  </div>
+                  <div className="pl-3 font-bold text-green-900">
+                    1 year access
+                  </div>
+                </div>
+              </ul>
             </div>
             <div className="flex flex-col bg-white rounded p-4 items-center shadow-lg border-r-8 border-red-400">
-              <h2 className="font-bold">Lorem Ipsum</h2>
-              <p className="p-4 text-gray-600">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
-                doloribus quia voluptates maxime totam illum iure, quis...
-              </p>
-              <div className="flex justify-between ">
-                <div className="pill bg-gray-400 rounded-full text-xs px-4 py-1 mr-2">
-                  #Express
-                </div>
-                <div className="pill bg-gray-400 rounded-full text-xs px-4 py-1 mr-2">
-                  #TailwindCSS
-                </div>
-              </div>
+              <button className="transition duration-500 border-1 text-lg h-12 w-80 bg-white hover:bg-red-500 hover:text-white text-red-500 mt-2 px-3 rounded-md">
+                <div>Download Course Brouchre</div>
+              </button>
             </div>
           </div>
         </div>
