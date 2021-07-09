b6c94ec66224933590a8062e36b60c198fade034
Sanchit Hajela <sanchit10hajela@gmail.com>
2021-07-09 12:11:23 +0530

    Minor change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 module.exports = {
-  purge: ["./src/*/.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    borderRadius: {
-      none: "0",
-      sm: "0.125rem",
-      DEFAULT: "0.25rem",
-      md: "0.375rem",
-      lg: "0.5rem",
-      full: "9999px",
-      large: "100px",
-    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
